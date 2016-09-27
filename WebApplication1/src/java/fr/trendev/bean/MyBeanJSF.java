eebb332884fc9a7f8732ce6c3089cf62248badf4
jsie <julien.sie@gmail.com>
2016-09-27 16:29:38 +0200

    AJAX & JSF #2

--- WebApplication1/src/java/fr/trendev/bean/MyBeanJSF.java
+++ WebApplication1/src/java/fr/trendev/bean/MyBeanJSF.java
@@ -33,6 +33,8 @@ public class MyBeanJSF implements Serializable {
     private int iter;
 
     private int incr;
+    
+    private final static int bound = 100;
 
     private static final Logger logger = Logger.getLogger("MyBeanJSF");
 
@@ -65,6 +67,12 @@ public class MyBeanJSF implements Serializable {
     public void setIncr(int incr) {
         this.incr = incr;
     }
+
+    public int getBound() {
+        return bound;
+    }
+    
+    
 
     @PostConstruct
     public void init() {
