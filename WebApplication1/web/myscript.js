9266bcbb0001ce5a7f6727d7aed060d49763287f
jsie <julien.sie@gmail.com>
2016-11-21 17:34:41 +0100

    font family + javascript

--- WebApplication1/web/myscript.js
+++ WebApplication1/web/myscript.js
@@ -1,8 +1,24 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Author : Julien SIE
  */
-alert("Access to index.html ;) ");
 
+function display_elts(selector) {
+
+    var qsa = document.querySelectorAll(selector);
+    var c = qsa.length;
+    var s = selector.split('.')[1];
+
+    if (!c) {
+        alert("No element with CSS class :  " + s + " !!!");
+    } else {
+        console.log("Will find all elements with CSS class : " + s);
+        console.log(c + " element" + ((c > 1) ? "s" : ""));
+
+        for (var i = 0; i < c; i++) {
+            console.log("Element " + i + " : " + qsa[i].innerHTML + " ==> " + qsa[i].href);
+        }
+    }
+}
+
+display_elts(".lnk");
 
