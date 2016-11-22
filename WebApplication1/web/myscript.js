2279a09e3dc1f2e5a12c49129035ef89bf025625
jsie <julien.sie@gmail.com>
2016-11-22 15:30:57 +0100

    JavaScript : random-shuffle the JSF/JSP/RestfulAPI links

--- WebApplication1/web/myscript.js
+++ WebApplication1/web/myscript.js
@@ -19,22 +19,49 @@ function display_elts(selector) {
     }
 }
 
-var shuffle_links = function () {
+/*
+ * Random-shuffle the different links. Display the links before and after the shuffle.
+ */
+var random_shuffle = function () {
     var selector = 'a';
     var links = document.querySelectorAll(selector);
-    var firstLink, parent, count;
 
     display_elts(selector);
 
-    count = links.length;
+    var count = links.length;
+
     if (count > 1) {
-        parent = links[0].parentNode;
-        firstLink = parent.removeChild(links[0]);
-        parent.appendChild(firstLink);
+        var parent = links[0].parentNode;
+
+        var newlinks = new Array(count);
+
+        /*
+         * Remove each element, put them in a tmp array... 
+         */
+        for (var i = 0; i < count; i++) {
+            var firstLink = parent.removeChild(links[i]);
+            //provide a number between 0 and (count-1).
+            var index = Math.floor(((Math.random() * (count - 1)) + 1));
+            //if the calculated position is not free, try to provide the next index.
+            while (newlinks[index]) {
+                console.log("index = " + index);
+                index = (index + 1) % count;
+                console.log("new index = " + index);
+            }
+            console.log("set " + firstLink.innerHTML + " in index " + index);
+            newlinks[index] = firstLink;
+        }
+        /*
+         * ... And then, add them from the tmp array to the parent element.
+         */
+        for (var i = 0; i < count; i++)
+            parent.appendChild(newlinks[i]);
+
+        display_elts(selector);
     }
 
-    display_elts(selector);
+
 
 };
 
-var myVar = setInterval(shuffle_links, 3000);
+var myVar = setInterval(random_shuffle, 10000);
