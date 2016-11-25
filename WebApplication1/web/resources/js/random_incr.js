2995000b004e8974909d4c1d65691803b55c45d9
jsie <julien.sie@gmail.com>
2016-11-25 19:15:47 +0100

    Implement a Counter before the random generator

--- WebApplication1/web/resources/js/random_incr.js
+++ WebApplication1/web/resources/js/random_incr.js
@@ -2,15 +2,27 @@
  * Author : Julien SIE
  */
 
+var d = delay = 5;
+var counter = document.getElementById('form1:counter');
+counter.innerHTML = delay;
+
 var random_generator = function () {
-    var incr = document.getElementById('form1:incr');
-    var bound = document.getElementById('form1:bound');
-    //TODO : use #{myBeanJSF.bound} instead of a hardcoded value 200.
-    incr.value = Math.floor((Math.random() * bound.value) + 1);
-    //Fire a change event and trigger the onChange() method provided with AJAX (the listener of the JSF component is a backend bean method.
-    incr.onchange();
+
+    counter.innerHTML = --d;
+
+    if (d === 0) {
+        var incr = document.getElementById('form1:incr');
+        var bound = document.getElementById('form1:bound');
+        incr.value = Math.floor((Math.random() * bound.value) + 1);
+        /*
+         * Fire a change event and trigger the onChange() method provided with AJAX (the listener of the JSF component is a backend bean method.
+         * */
+        incr.onchange();
+        d = delay;
+        counter.innerHTML = delay;
+    }
 };
 
-setInterval(random_generator, 5000);
+setInterval(random_generator, 1000);
 
 
