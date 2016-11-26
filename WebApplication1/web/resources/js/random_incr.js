dc268b3b7d729be57f95b4278df8ff4ec517c45c
jsie <julien.sie@gmail.com>
2016-11-26 11:30:58 +0100

    javascript : refactoring

--- WebApplication1/web/resources/js/random_incr.js
+++ WebApplication1/web/resources/js/random_incr.js
@@ -2,27 +2,41 @@
  * Author : Julien SIE
  */
 
-var d = delay = 5;
-var counter = document.getElementById('form1:counter');
-counter.innerHTML = delay;
+var ri = {
+    delay: 5,
 
-var random_generator = function () {
+    start: function () {
+        var d = ri.delay;
+        var counter = document.getElementById('form1:counter');
+        counter.innerHTML = d;
 
-    counter.innerHTML = --d;
+        var random_generator = function () {
 
-    if (d === 0) {
-        var incr = document.getElementById('form1:incr');
-        var bound = document.getElementById('form1:bound');
-        incr.value = Math.floor((Math.random() * bound.value) + 1);
-        /*
-         * Fire a change event and trigger the onChange() method provided with AJAX (the listener of the JSF component is a backend bean method.
-         * */
-        incr.onchange();
-        d = delay;
-        counter.innerHTML = delay;
+            counter.innerHTML = --d;
+
+            if (d === 0) {
+                var incr = document.getElementById('form1:incr');
+                /*The hidden data in the Facelet...*/
+                var bound = document.getElementById('form1:bound');
+
+                incr.value = Math.floor((Math.random() * bound.value) + 1);
+
+                /*
+                 * Fire a change event and trigger the onChange() method provided with AJAX (the listener of the JSF component is a backend bean method.
+                 * */
+                incr.onchange();
+
+                d = ri.delay;
+                counter.innerHTML = d;
+            }
+        };
+
+        setInterval(random_generator, 1000);
     }
 };
 
-setInterval(random_generator, 1000);
+ri.delay = 10;
+ri.start();
+
 
 
--- WebApplication1/web/resources/js/shuffle_links.js
+++ WebApplication1/web/resources/js/shuffle_links.js
@@ -2,80 +2,84 @@
  * Author : Julien SIE
  */
 
-function display_elts(selector) {
+if (typeof sl === 'undefined') {
+    var sl = {
 
-    var qsa = document.querySelectorAll(selector);
-    var c = qsa.length;
+        delay: 2000,
 
-    if (!c) {
-        alert("No element with CSS class :  " + selector + " !!!");
-    } else {
-        console.log("Will find all elements with CSS class : " + selector);
-        console.log(c + " element" + ((c > 1) ? "s" : ""));
+        selector: 'a',
 
-        for (var i = 0; i < c; i++) {
-            console.log("Element " + i + " : " + qsa[i].innerHTML + " ==> " + qsa[i].href);
+        display_elts: function () {
+
+            var qsa = document.querySelectorAll(sl.selector);
+            var c = qsa.length;
+            if (!c) {
+                alert("No element with CSS class :  " + sl.selector + " !!!");
+            } else {
+                console.log("Will find all elements with CSS class : " + sl.selector);
+                console.log(c + " element" + ((c > 1) ? "s" : ""));
+                for (var i = 0; i < c; i++) {
+                    console.log("Element " + i + " : " + qsa[i].innerHTML + " ==> " + qsa[i].href);
+                }
+            }
+        },
+        /*
+         * Random-shuffle the different links. Display the links before and after the shuffle.
+         */
+        random_shuffle: function () {
+            var links = document.querySelectorAll(sl.selector);
+            //sl.display_elts();
+
+            var count = links.length;
+            if (count > 1) {
+                var parent = links[0].parentNode;
+                var newlinks = new Array(count);
+                /*
+                 * Remove each element, put them in a tmp array... 
+                 */
+                for (var i = 0; i < count; i++) {
+                    var link = parent.removeChild(links[i]);
+                    //provide a number between 0 and (count-1).
+                    var index = Math.floor(((Math.random() * (count - 1)) + 1));
+                    //if the calculated position is not free, try to provide the next index.
+                    while (newlinks[index]) {
+                        index = (index + 1) % count;
+                    }
+                    console.log("set " + link.innerHTML + " in index " + index);
+                    newlinks[index] = link;
+                }
+
+                /*
+                 * ... And then, add them from the tmp array to the parent element.
+                 */
+                for (var i = 0; i < count; i++)
+                    parent.appendChild(newlinks[i]);
+                // sl.display_elts();
+            }
+
+
+
+        },
+
+        start: function () {
+            setInterval(sl.random_shuffle, sl.delay);
+
+            var pos = document.getElementById('pos');
+            var MSG = " - ; - ";
+            pos.innerHTML = MSG;
+
+            window.addEventListener('mousemove', function (e) {
+                pos.innerHTML = "X = " + e.clientX + "px ; Y = " + e.clientY + "px";
+            }, false);
+
+            window.addEventListener('mouseout', function (e) {
+                pos.innerHTML = MSG;
+            }, false);
         }
-    }
+    };
+} else {
+    alert("ERROR : 'sl' namespace in shuffle_links.js already exists !");
 }
 
-/*
- * Random-shuffle the different links. Display the links before and after the shuffle.
- */
-var random_shuffle = function () {
-    var selector = 'a';
-    var links = document.querySelectorAll(selector);
-
-    //display_elts(selector);
-
-    var count = links.length;
-
-    if (count > 1) {
-        var parent = links[0].parentNode;
-
-        var newlinks = new Array(count);
-
-        /*
-         * Remove each element, put them in a tmp array... 
-         */
-        for (var i = 0; i < count; i++) {
-            var link = parent.removeChild(links[i]);
-            //provide a number between 0 and (count-1).
-            var index = Math.floor(((Math.random() * (count - 1)) + 1));
-            //if the calculated position is not free, try to provide the next index.
-            while (newlinks[index]) {
-                console.log("index = " + index);
-                index = (index + 1) % count;
-                console.log("new index = " + index);
-            }
-            console.log("set " + link.innerHTML + " in index " + index);
-            newlinks[index] = link;
-        }
-
-        /*
-         * ... And then, add them from the tmp array to the parent element.
-         */
-        for (var i = 0; i < count; i++)
-            parent.appendChild(newlinks[i]);
-
-        // display_elts(selector);
-    }
-
-
-
-};
-
-setInterval(random_shuffle, 3000);
-
-var div = document.getElementById('div');
-var pos = document.getElementById('pos');
-var MSG = " - ; - ";
-pos.innerHTML = MSG;
-
-window.addEventListener('mousemove', function (e) {
-    pos.innerHTML = "X = " + e.clientX + "px ; Y = " + e.clientY + "px";
-}, false);
-
-window.addEventListener('mouseout', function (e) {
-    pos.innerHTML = MSG;
-}, false);
+sl.delay = 1000;
+sl.start();
