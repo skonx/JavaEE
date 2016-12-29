a6063e3e3f4ec05acbb33c3a0d06b5f1ed074a62
jsie <julien.sie@gmail.com>
2016-12-29 19:38:06 +0100

    add angularjs animation

--- WebApplication1/web/resources/js/angular_test.js
+++ WebApplication1/web/resources/js/angular_test.js
@@ -1,10 +1,17 @@
-var app = angular.module("myWebApp1", []);
+var app = angular.module("myWebApp1", ['ngAnimate']);
 
 app.controller("ValueCtrl", function ($scope) {
     console.warn("# Controller ValueCtrl : loaded #");
 
     function check() {
-        console.warn("Angular CHECK() - Value incr changed : " + $scope.incr);
+        console.warn("Angular CHECK() - Value incr changed : " + $scope.incr + ' and Bound is still ' + $scope.bound);
+
+        //the background-color of the fourth progress bar will be overriden and fade using the incr computed value
+        var incrset = document.getElementById('form1:pbar4:bar');
+
+        var red = Math.floor(($scope.incr * 255) / $scope.bound) % 256;
+        angular.element(incrset).css("background-color", "rgb(" + red + ",0,0)");
+
     }
 
     //won't be called if the value is changed from outside of the scope !
