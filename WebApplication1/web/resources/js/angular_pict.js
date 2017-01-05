56b6cbc4921b86ebb34497a89bafc5eda599e0ae
jsie <julien.sie@gmail.com>
2017-01-05 22:21:55 +0100

    init directive

--- WebApplication1/web/resources/js/angular_pict.js
+++ WebApplication1/web/resources/js/angular_pict.js
@@ -3,12 +3,6 @@ var app = angular.module("pictTest", []);
 app.controller("ImgSrcCtrl", function ($scope) {
     $scope.pict = '';
     var path = "http://localhost:8080/WebApplication1/webresources/images/";
-    $scope.uploading = false;
-
-    /**
-     * By default, the picture will be cached on the client side.
-     */
-    //$scope.picturl = path + $scope.pict;
 
     /**
      * 
@@ -18,58 +12,68 @@ app.controller("ImgSrcCtrl", function ($scope) {
     $scope.setUrl = function () {
         if ($scope.pict)
             $scope.picturl = path + $scope.pict;
+        console.log("URL updated and $scope.pict = [" + $scope.pict + "]");
     };
+});
 
-    var fileInput = document.querySelector('#file');
-    var progress = document.querySelector('#progress');
-
-    fileInput.addEventListener('change', function () {
-
-        var xhr = new XMLHttpRequest();
-
-        /* $scope.uploading is not accessible from javascript event handler.
-         * This function will switch the uploading value in order to 
-         * display (or not) a progress bar
-         * */
-        var switch_uploading = function () {
-            var upload = document.getElementById('upload');
-            var scope = angular.element(upload).scope();
-            scope.$apply(scope.uploading = !scope.uploading);
-        };
-
-        switch_uploading();
-
-        xhr.open('POST', path);
-
-        xhr.upload.addEventListener('progress', function (e) {
-            progress.value = e.loaded;
-            progress.max = e.total;
-        });
-
-        xhr.addEventListener('abort', function () {
-            switch_uploading();
-            alert('Upload aborted !');
-        });
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status < 400) {
-                    alert('Upload is over ! ');
-                    console.log("xhr.responseText = " + xhr.responseText);
-                    $scope.pict = xhr.responseText;
-                } else {
-                    var errmsg = '[ Error ] Upload failed\n';
-                    errmsg += 'Status : ' + xhr.status + ' ' + xhr.statusText;
-                    alert(errmsg);
-                }
-
-                switch_uploading();
+app.directive('trdvUploader', [function () {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                pict: '=picture',
+                fn: '&callbackFn'
+            },
+            templateUrl: 'resources/template/uploader.html',
+            link: function (scope, element, attrs, controller, transcludeFn) {
+                scope.uploading = false;
+                scope.pict = '8e22e2e1-e52b-4e02-a630-45a5c0e03d79.png';
+                scope.fn();
             }
         };
+    }]);
 
-        /*var form = new FormData();
-         form.append('file', fileInput.files[0]);*/
-
-        xhr.send(fileInput.files[0]);
-    });
-});
+/*var fileInput = document.querySelector('#file');
+ var progress = document.querySelector('#progress');
+ 
+ fileInput.addEventListener('change', function () {
+ 
+ var xhr = new XMLHttpRequest();
+ var switch_uploading = function () {
+ var upload = document.getElementById('upload');
+ var scope = angular.element(upload).scope();
+ scope.$apply(scope.uploading = !scope.uploading);
+ };
+ 
+ switch_uploading();
+ 
+ xhr.open('POST', path);
+ 
+ xhr.upload.addEventListener('progress', function (e) {
+ progress.value = e.loaded;
+ progress.max = e.total;
+ });
+ 
+ xhr.addEventListener('abort', function () {
+ switch_uploading();
+ alert('Upload aborted !');
+ });
+ 
+ xhr.onreadystatechange = function () {
+ if (xhr.readyState === XMLHttpRequest.DONE) {
+ if (xhr.status < 400) {
+ alert('Upload is over ! ');
+ console.log("xhr.responseText = " + xhr.responseText);
+ $scope.pict = xhr.responseText;
+ } else {
+ var errmsg = '[ Error ] Upload failed\n';
+ errmsg += 'Status : ' + xhr.status + ' ' + xhr.statusText;
+ alert(errmsg);
+ }
+ 
+ switch_uploading();
+ }
+ };
+ 
+ xhr.send(fileInput.files[0]);
+ });*/
