dff1b84015f14e830f7ed2a15c0c194f403b34b9
jsie <julien.sie@gmail.com>
2017-01-12 22:55:38 +0100

    Filter now also supports asynchronous request

--- WebApplication1/src/java/fr/trendev/web/filter/MyFilter.java
+++ WebApplication1/src/java/fr/trendev/web/filter/MyFilter.java
@@ -16,6 +16,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -26,7 +27,7 @@ import javax.servlet.http.HttpSession;
  */
 /*servletNames = {"Faces Servlet", "javax.ws.rs.core.Application",
     "MyServletTest"}*/
-//@WebFilter(urlPatterns = {"/*"}, asyncSupported = true)
+@WebFilter(urlPatterns = {"/*"}, asyncSupported = true)
 public class MyFilter implements Filter {
 
     @EJB
